b48e8857b446a69bcac9e30af78f46974e9c8134
Łukasz Pięta <pieta.lukasz98@gmail.com>
2020-05-10 01:18:08 +0200

    Remove previous days' trips after commit performed

--- src/com/bigdata/consumer/CustomTransformer.kt
+++ src/com/bigdata/consumer/CustomTransformer.kt
@@ -10,12 +10,12 @@ import org.apache.kafka.streams.kstream.Transformer
 import org.apache.kafka.streams.processor.ProcessorContext
 import org.apache.kafka.streams.processor.PunctuationType
 import org.apache.kafka.streams.state.KeyValueStore
+import java.time.LocalDate
 
 class CustomTransformer(
     private val stateStoreName: String
 ) : Transformer<String, String, KeyValue<String, String>> {
 
-    private var aggregatedInfo: AggregatedInfo = AggregatedInfo()
     private var context: ProcessorContext? = null
     private var kvStore: KeyValueStore<String, String>? = null
 
@@ -28,31 +28,37 @@ class CustomTransformer(
         val aggr: String? = kvStore?.get(kvStoreKey.toString())
         val existingValue = aggr?.let { jsonMapper.readValue(it, AggregatedInfo::class.java) }
 
-        val updatedValue = (existingValue ?: AggregatedInfo(consumerDateKey = kvStoreKey)).let {
-            when (tripStation.tripType) {
-                1 -> it.copy(startedTrips = it.startedTrips + 1L)
-                else -> it.copy(endedTrips = it.endedTrips + 1L)
+        val updatedValue =
+            (existingValue ?: AggregatedInfo(consumerDateKey = kvStoreKey, updateTime = tripStation.tripTime)).let {
+                when (tripStation.tripType) {
+                    1 -> it.copy(startedTrips = it.startedTrips + 1L)
+                    else -> it.copy(endedTrips = it.endedTrips + 1L)
+                }
+                    .copy(
+                        avgTemperature = ((it.endedTrips + it.startedTrips) * it.avgTemperature + tripStation.temperature) /
+                                (it.endedTrips + it.startedTrips + 1),
+                        updateTime = tripStation.tripTime
+                    )
             }
-                .copy(
-                    avgTemperature = ((it.endedTrips + it.startedTrips) * it.avgTemperature + tripStation.temperature) /
-                            (it.endedTrips + it.startedTrips + 1)
-                )
-        }
 
         kvStore?.put(kvStoreKey.toString(), updatedValue.toString())
-        //TODO delete past requests?
-        return null //KeyValue(kvStoreKey.toString(), updatedValue.toString())
+        return null
     }
 
     override fun init(context: ProcessorContext) {
         this.context = context
         this.kvStore = context.getStateStore(stateStoreName) as KeyValueStore<String, String>
 
+        var lastDate: LocalDate?
+
+        val entryDateList: MutableList<LocalDate> = mutableListOf()
+
         this.context!!.schedule(300_000, PunctuationType.STREAM_TIME) {
-            val iter = kvStore!!.all()
+            var iter = kvStore!!.all()
             while (iter.hasNext()) {
                 val entry: KeyValue<String, String>? = iter.next()
                 if (entry != null) {
+                    entryDateList.add(jsonMapper.readValue(entry.key, ConsumerDateKey::class.java).eventDay)
                     context.forward(entry.key, entry.value.toString())
                 }
             }
@@ -60,6 +66,19 @@ class CustomTransformer(
 
             // commit the current processing progress
             context.commit()
+
+            // delete these values that are already committed and from previous days
+            lastDate = entryDateList.maxBy { it }
+            iter = kvStore!!.all()
+            while (iter.hasNext()) {
+                val entry: KeyValue<String, String>? = iter.next()
+                if (entry != null) {
+                    val entryDate = jsonMapper.readValue(entry.key, ConsumerDateKey::class.java).eventDay
+                    if (entryDate.isBefore(lastDate))
+                        kvStore!!.delete(entry.key)
+                }
+            }
+
         }
     }
 
--- src/com/bigdata/model/AggregatedInfo.kt
+++ src/com/bigdata/model/AggregatedInfo.kt
@@ -1,12 +1,14 @@
 package com.bigdata.model
 
 import com.bigdata.lib.jsonMapper
+import java.time.LocalDateTime
 
 data class AggregatedInfo(
-    val consumerDateKey: ConsumerDateKey? = ConsumerDateKey(),
+    val consumerDateKey: ConsumerDateKey = ConsumerDateKey(),
     val avgTemperature: Double = 0.0,
     val startedTrips: Long = 0L,
-    val endedTrips: Long = 0L
+    val endedTrips: Long = 0L,
+    val updateTime: LocalDateTime
 ) {
     override fun toString(): String {
         return jsonMapper.writeValueAsString(this)
